ab7f18830fc82e171c45e35b8f5ccf36b21b947f
nicoContigliani <nico.contigliani@gmail.com>
2022-11-24 11:09:56 -0300

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import React, { Fragment, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+type FormElement = React.FormEvent<HTMLFormElement>
+interface ITask {
+  name: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>('')
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault()
+    addTask(newTask)
+    setNewTask('')
+    console.log(tasks)
+  }
+
+  const addTask = (name: string) => {
+    const newTasks: ITask[] = [...tasks, { name, done: false }]
+    setTasks(newTasks)
+  }
+  const toggleDonTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks)
+  }
+  const removeTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="card">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <input type="text" onChange={e => setNewTask(e.target.value)} value={newTask} className="form-control"
+              autoFocus
+            />
+            <button type="submit">Send</button>
+          </form>
+
+        </div>
+      </div>
+      {
+        tasks.map((t: ITask, i: number) => (
+          <div key={i} className="card card-body mt-2 ">
+            <h2
+              style={{ textDecoration: t.done ? 'line-through' : "" }}
+            >{t.name}</h2>
+            <button className="btn btn-primary" onClick={() => toggleDonTask(i)}>
+              {t.done ? '✔' : '✗'}
+            </button>
+            <button
+                      onClick={() => removeTask(i)}
+                      className="btn btn-danger"
+                    >
+                      🗑
+                    </button>
+
+          </div>
+        ))
+      }
+    </Fragment>
   );
 }
 
